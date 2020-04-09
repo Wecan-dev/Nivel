75c57632b26741a7f54b7c18a9a2eced4dbfa230
Gladibeth <gladibethdelosa@gmail.com>
2020-04-09 19:52:59 -0400

    Add maps and form projects and add form admin

--- wp-content/themes/Nivel-w/partials/project/contact-map.php
+++ wp-content/themes/Nivel-w/partials/project/contact-map.php
@@ -6,7 +6,9 @@
           <img class="img-icono__ubicacion" src="<?php echo get_template_directory_uri();?>/assets/img/logo-3.png">
         </div>
       </div>
-      <img class="img-ubicacion" src="<?php echo get_template_directory_uri();?>/assets/img/imagen-ubicacion.png">
+      <iframe style="width: 100%;height: 100%;" 
+    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3966.4497363100777!2d-75.57303108600856!3d6.204252595508624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sCra%2042A%20%23%201-15%2C%20Torre%204%2C%20Oficina%20307%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20San%20Fernando%20Plaza%2C%20Medell%C3%ADn%2C%20CO!5e0!3m2!1ses!2sve!4v1584460257028!5m2!1ses!2sve"
+    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
     <div class="main-details__ubicacion--form">
       <div class="main-contactform__box">
@@ -16,6 +18,8 @@
           <br>
           información de este proyecto
         </h2>
+        <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 3, 'title' => false, 'description' => false ) ); ?>
+        <!--
         <form action="">
           <div class="form-group">
             <input class="form-control" id="formGroupExampleInput" placeholder="Nombre" type="text">
@@ -48,7 +52,7 @@
               Enviar
             </button>
           </div>
-        </form>
+        </form>-->
       </div>
     </div>
   </div>
