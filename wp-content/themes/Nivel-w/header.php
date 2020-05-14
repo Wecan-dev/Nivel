cc2a7063caf0e0f5c9bb0569bf15a43fca8e77c7
Yonnys Quintero <leafar.qr@gmail.com>
2020-05-14 11:35:54 -0400

    header

--- wp-content/themes/Nivel-w/header.php
+++ wp-content/themes/Nivel-w/header.php
@@ -15,36 +15,36 @@
 	<meta name='keywords', content=''>
 	<meta name="robots" content="index, follow">
 
-		<!-- Blog Meta Data -->
-	<meta name="dc.language" content="es">
-	<meta name="dc.source" content="<?php bloginfo('url');?>">
-	<meta itemprop="url" content="<?php bloginfo('url');?>">
+  <!-- Blog Meta Data -->
+  <meta name="dc.language" content="es">
+  <meta name="dc.source" content="<?php bloginfo('url');?>">
+  <meta itemprop="url" content="<?php bloginfo('url');?>">
 
-	<!-- Twitter Card Meta Data -->
-	<meta content='summary' name='twitter:card'>
-	<meta content='Paginas web' name='twitter:site'>
-	<meta content='Paginas web' name='twitter:title'>
-	<meta content='Paginas web' name='twitter:description'>
+  <!-- Twitter Card Meta Data -->
+  <meta content='summary' name='twitter:card'>
+  <meta content='Paginas web' name='twitter:site'>
+  <meta content='Paginas web' name='twitter:title'>
+  <meta content='Paginas web' name='twitter:description'>
 
-	<!-- Open Graph Meta Data -->
-	<meta content='website' property='og:type'>
-	<meta content='<?php echo get_template_directory_uri(); ?>/assets/img/logo-header.png' property='og:image'>
-	<meta property="og:site_name" content="">
-	<meta property="og:title" content="">
-	<meta content='' property='og:description'>
-	<meta property="og:type" content="">
-	<meta property="og:image" content="">
+  <!-- Open Graph Meta Data -->
+  <meta content='website' property='og:type'>
+  <meta content='<?php echo get_template_directory_uri(); ?>/assets/img/logo-header.png' property='og:image'>
+  <meta property="og:site_name" content="">
+  <meta property="og:title" content="">
+  <meta content='' property='og:description'>
+  <meta property="og:type" content="">
+  <meta property="og:image" content="">
 
   <!-- Links -->
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/offcanvas.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/slick.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/slick-theme.css">
-	<link rel="stylesheet" type="text/css"
-		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/main.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/contacto.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/responsive.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/offcanvas.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/slick.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/slick-theme.css">
+  <link rel="stylesheet" type="text/css"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/main.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/contacto.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/assets/css/responsive.css">
   <!-- <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet"> -->
   <?php wp_head(); ?>
 </head>
@@ -60,16 +60,16 @@
             <img class="img-logo" src="<?php echo get_template_directory_uri();?>/assets/img/logo.png">
           </a>
           <button class="navbar-toggler p-2  hamburger hamburger--elastic ml-autos" data-toggle="offcanvas"
-            type="button">
-            <span class="hamburger-box">
-              <span class="hamburger-inner"></span>
-            </span>
+          type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
 
-          </button>
-        </div>
+        </button>
+      </div>
 
-        <div class="navbar-collapse offcanvas-collapse">
-          <div class="navbar-nav mr-autos">
+      <div class="navbar-collapse offcanvas-collapse">
+        <div class="navbar-nav mr-autos">
             <!-- <a href="< ?php echo bloginfo('url').'/';?>" class="main-logo__collapse">
               <img class="logo-collapse" src="< ?php echo get_template_directory_uri();?>/assets/img/logo_4.png">
             </a> -->
@@ -92,23 +92,23 @@
             <!--<li class="nav-item">
               <a class="nav-link nav-link__mobile" href="< ?php echo bloginfo('url').'/inversion';?>">invierte desde el
                 exterior</a>
-            </li>-->
-            <li class="nav-item">
-              <a class="nav-link " href="<?php echo bloginfo('url').'/contacto';?>">contacto</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.facebook.com/nivelpropiedadraiz" target="_blank" class="mr-3">
-                <img src="<?php echo get_template_directory_uri();?>/assets/img/fb_2.png" alt="">
+              </li>-->
+              <li class="nav-item">
+                <a class="nav-link " href="<?php echo bloginfo('url').'/contacto';?>">contacto</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://www.facebook.com/nivelpropiedadraiz" target="_blank" class="mr-3">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/img/fb_2.png" alt="">
 
-              </a>
-              <a href="https://www.instagram.com/nivelpropiedadraiz/" target="_blank" class="mr-3">
-                <img src="<?php echo get_template_directory_uri();?>/assets/img/instagram.png" alt="">
+                </a>
+                <a href="https://www.instagram.com/nivelpropiedadraiz/" target="_blank" class="mr-3">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/img/instagram.png" alt="">
 
-              </a>
-            </li>
+                </a>
+              </li>
+            </div>
           </div>
         </div>
-      </div>
 
-    </nav>
-	</header>
+      </nav>
+    </header>
