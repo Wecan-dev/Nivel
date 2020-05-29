e9884f28ba0dbd69248418ac0a30ef2ccb62ec02
Wecan Company <contacto@wecan.company>
2020-05-29 17:11:23 +0000

    Add change in productions

--- wp-config.php
+++ wp-config.php
@@ -24,10 +24,10 @@ define('FS_METHOD','direct');
 define( 'DB_NAME', 'db_nivel' );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+define( 'DB_USER', 'adminwecan' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', '_*8gTYWqM9FHU' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
